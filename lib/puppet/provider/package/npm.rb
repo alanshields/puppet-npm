995d33a6a78df45104dddf41bb35281e24b3c279
narkisr <narkisr@gmail.com>
2011-05-18 02:33:18 +0300

    adjusting to current npm methods

--- lib/puppet/provider/package/npm.rb
+++ lib/puppet/provider/package/npm.rb
@@ -7,18 +7,16 @@ Puppet::Type.type(:package).provide :npm, :parent => Puppet::Provider::Package d
 #   raise Puppet::Error, "The npm provider can only be used as root" if Process.euid != 0
 
   def self.exec_as_user(op, pkg)
-    Puppet::Util::SUIDManager.asuser("node", "node") do 
-      s = execute ["sudo","/opt/node/bin/node", "/opt/node/lib/npm/cli.js", "#{op}", "#{pkg}"]
+      s = execute ["/opt/node/bin/node", "/opt/node/lib/npm/cli.js", "#{op}", "-g", "#{pkg}"]
       s.split("\n").collect do | line |
         yield line
       end
-    end
   end
 
   def self.npm_list(hash)
     begin
       list = []
-      exec_as_user("list", "installed") do | line |
+      exec_as_user("list", "") do | line |
         if npm_hash = npm_split(line)
           npm_hash[:provider] = :npm
           if (npm_hash[:name] == hash[:justme]) or hash[:local]
@@ -38,8 +36,9 @@ Puppet::Type.type(:package).provide :npm, :parent => Puppet::Provider::Package d
   end
 
   def self.npm_split(desc)
+    return nil if not desc.include? '@'
     split_desc = desc.split(/ /)
-    installed = split_desc[0]
+    installed = split_desc[1]
     name = installed.split(/@/)[0]
     version = installed.split(/@/)[1]
     if (name.nil? || version.nil?)
